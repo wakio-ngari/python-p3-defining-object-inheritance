38d3d33a8846d1a25b15fa42fa3581947fd0e151
wakio-ngari <ngarijoyce017@gmail.com>
2025-02-17 11:55:37 +0300

    completed object inheritance python lab

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,9 @@
-from vehicle import Vehicle
+from vehicle import Vehicle  # Importing the Vehicle class
 
-class Car(Vehicle):
-    pass
+class Car(Vehicle):  # Inheriting from Vehicle
+    def __init__(self, wheel_size, wheel_number, make=None):  # make has a default value
+        super().__init__(wheel_size, wheel_number)  # Calling the parent constructor
+        self.make = make  # New attribute specific to Car
+
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"  # Overriding the go() method
